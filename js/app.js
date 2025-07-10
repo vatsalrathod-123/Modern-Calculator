6f61a1971aba4b114b4a05565182a32e92eec7a2
vatsalsrathod-123 <vatsalsrathod123@gmail.com>
2025-07-10 10:19:20 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let currentExpression = "";
+
+function appendNumber(number) {
+  currentExpression += number;
+  document.getElementById("result").value = currentExpression;
+}
+
+function appendOperator(operator) {
+  currentExpression += `${operator}`;
+  document.getElementById("result").value = currentExpression;
+}
+
+function calculateResult() {
+  try {
+    currentExpression = eval(
+      currentExpression.replace("/", "/").replace("x", "*")
+    );
+    document.getElementById("result").value = currentExpression;
+  } catch {
+    document.getElementById("result").value = "Error";
+  }
+}
+
+function clearResult() {
+  currentExpression = "";
+  document.getElementById("result").value = "";
+}
